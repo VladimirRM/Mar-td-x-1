ec34249424afc483e72df1246696e05d88988a91
Vladimir RM <shvdrummer@gmail.com>
2022-12-01 10:37:02 +0200

    create main  const

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const btn = document.querySelector('#btn')
+const input = document.querySelector('#input')
+const result = document.querySelector('#result')
+const total = document.querySelector('#total')
