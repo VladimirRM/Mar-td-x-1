a4142a583d44c1b8255ac8b454765288064e0504
Vladimir RM <shvdrummer@gmail.com>
2022-12-01 10:39:20 +0200

    create block input

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-const btn = document.querySelector('#btn')
-const input = document.querySelector('#input')
-const result = document.querySelector('#result')
-const total = document.querySelector('#total')
+const btn = document.querySelector("#btn");
+const input = document.querySelector("#input");
+const result = document.querySelector("#result");
+const total = document.querySelector("#total");
+
+btn.addEventListener("click", (e) => {
+  if (input.value === "") return;
+  createDeleteElements(input.value);
+  input.value = "";
+});
+
+
+
