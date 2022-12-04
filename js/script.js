c6ecc09939115db5a74f59ffe1c78983feda7ab9
Vladimir RM <shvdrummer@gmail.com>
2022-12-04 10:19:24 +0200

    create btn for li

--- js/script.js
+++ js/script.js
@@ -4,14 +4,19 @@ const result = document.querySelector("#result");
 const total = document.querySelector("#total");
 let i = 0;
 
-btn.addEventListener('click',(value)=>{
-  if(input.value==='')return
-  createDeleteElements(input.value)
-  input.value = ''
-})
+btn.addEventListener("click", (value) => {
+  if (input.value === "") return;
+  createDeleteElements(input.value);
+  input.value = "";
+});
 
-function createDeleteElements(e){
-    const li = document.createElement('li')
+function createDeleteElements(e) {
+  const li = document.createElement("li");
+  li.className = "li";
+  li.textContent = input.value;
+  const btn = document.createElement("li");
+  li.className = "li";
+  li.textContent = input.value;
+
+  result.appendChild(li)
 }
-
-
