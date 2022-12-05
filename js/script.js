f3033e9903a525fad8dc0cf6bbeae076289904cd
Vladimir RM <shvdrummer@gmail.com>
2022-12-05 09:43:04 +0200

    get value from input

--- js/script.js
+++ js/script.js
@@ -4,19 +4,8 @@ const result = document.querySelector("#result");
 const total = document.querySelector("#total");
 let i = 0;
 
-btn.addEventListener("click", (value) => {
-  if (input.value === "") return;
+btn.addEventListener("click", (e) => {
+  if (input.value) return;
   createDeleteElements(input.value);
   input.value = "";
 });
-
-function createDeleteElements(e) {
-  const li = document.createElement("li");
-  li.className = "li";
-  li.textContent = input.value;
-  const btn = document.createElement("li");
-  li.className = "li";
-  li.textContent = input.value;
-
-  result.appendChild(li)
-}
