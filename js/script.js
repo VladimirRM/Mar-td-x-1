1d5194d3102cf2a50bcf793bc052abc618ee20ba
Vladimir RM <shvdrummer@gmail.com>
2022-12-06 13:50:41 +0200

    clear old code

--- js/script.js
+++ js/script.js
@@ -4,26 +4,3 @@ const result = document.querySelector("#result");
 const total = document.querySelector("#total");
 let i = 0;
 
-btn.addEventListener("click", (e) => {
-  if (input.value === "") return;
-  createDeleteElements(input.value);
-  input.value = "";
-});
-
-function createDeleteElements(value) {
-  const li = document.createElement("li");
-  li.className = "li";
-  li.textContent = value;
-  const btn = document.createElement("button");
-  btn.className = "btn";
-  btn.textContent = "Delete";
-  li.appendChild(btn);
-  result.appendChild(li);
-
-  btn.addEventListener("click", (e) => {
-    result.removeChild(li);
-  });
-  li.addEventListener("click", (e) => {
-    li.classList.toggle("li-active");
-  });
-}
